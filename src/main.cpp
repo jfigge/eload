06663e19b5e639b717c2a1961fefe6f1ebf75003
Jason Figge <jason@sahara.lan>
2022-09-18 16:34:57 -0400

    tidied up.  Ready for load build out

--- src/main.cpp
+++ src/main.cpp
@@ -5,21 +5,33 @@
 #include <Adafruit_ADS1X15.h>
 #include <Adafruit_MCP4725.h>
 
-// #define PIN_SCL 5
-// #define PIN_SDA 4
-// #define PIN_A 2
-// #define PIN_B 1
-// #define PIN_SW 0
+#ifdef TINY
+// AT85
+#define PIN_SCL 2
+#define PIN_SDA 0
+#define PIN_A 3
+#define PIN_B 4
+#define PIN_SW 1
+#define PIN_PORT PINB
+#define SW_BYTE B00000010
+#define AB_BYTE B00011000 
+#define AB_SHIFT 3
+#else
+// Nano
 #define PIN_SCL A5
 #define PIN_SDA A4
-#define PIN_A 3
-#define PIN_B 2
+#define PIN_A 2
+#define PIN_B 3
 #define PIN_SW 4
-
+#define PIN_PORT PIND
+#define SW_BYTE B00010000
+#define AB_BYTE B00001100
+#define AB_SHIFT 2
+#endif
 
 LiquidCrystal_I2C lcd(0x27,20,4);  // Set the LCD address to 0x27 for a 16 chars and 2 line display
 Adafruit_ADS1115 ads;
-Adafruit_MCP4725 dac;
+// Adafruit_MCP4725 dac;
 
 float vin;
 float ain1;
@@ -37,7 +49,7 @@ RotataryHandler rfunc = 0;
 void REUpdateISR() {
   static uint8_t states[] = {0,1,4,0, 0,1,1,2, 2,1,3,2, 0,3,3,2, 0,4,4,5, 5,6,4,5, 0,6,6,5};
   static uint8_t last_state = 0;
-  uint8_t state = states[((PIND & B00001100) >> 2) | (last_state << 2)];
+  uint8_t state = states[((PIN_PORT & AB_BYTE) >> AB_SHIFT) | (last_state << 2)];
   if (rfunc != 0 && state == 0) {
     if (last_state == 6) {
       rfunc(-1);
@@ -66,22 +78,13 @@ void setup() {
   // *********************
   Serial.begin(BAUD_RATE);
 
-  dac.begin(0x60);
-  dac.setVoltage(0, false);
+  // dac.begin(0x60);
+  // dac.setVoltage(0, false);
 
   // Print a message to the LCD.
   lcd.init();                      // initialize the lcd 
   lcd.createChar(0, degree);
   lcd.backlight();
-  lcd.setCursor(2,0);
-  lcd.print("Electronic Load");
-  lcd.setCursor(4,1);
-  lcd.print("Jason Figge");
-  lcd.setCursor(1,2);
-  lcd.print("Version 0.0.3 Beta");
-  lcd.setCursor(1,3);
-  lcd.print("Built: Sep 11, 2022");
-
 
   // The ADC input range (or gain) can be changed via the following
   // functions, but be careful never to exceed VDD +0.3V max, or to
@@ -96,22 +99,38 @@ void setup() {
   // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
   // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
   if (!ads.begin()) {
+    lcd.clear();
     lcd.setCursor(0,0);
     lcd.print("Failed to initialize ADS.");
-    while (1);
+    delay(100);
+    while (1) {
+      delay(1000);
+    }
   }
 
   pinMode(PIN_A, INPUT);
   pinMode(PIN_A, INPUT);
   pinMode(PIN_SW, INPUT_PULLUP);
+  digitalWrite(5, LOW);
+  pinMode(5, OUTPUT);
+  #ifndef TINY
   attachInterrupt(digitalPinToInterrupt(PIN_A), REUpdateISR, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN_B), REUpdateISR, CHANGE);
+  #endif
   rfunc = rotaryHandler;
 
   // Display the splash screen for 4 seconds, or until the rotary
   // encoder is depressed
+  lcd.setCursor(2,0);
+  lcd.print("Electronic Load");
+  lcd.setCursor(4,1);
+  lcd.print("Jason Figge");
+  lcd.setCursor(1,2);
+  lcd.print("Version 0.0.4 Beta");
+  lcd.setCursor(1,3);
+  lcd.print("Built: Sep 16, 2022");  
   unsigned long SlapshStartTime = millis();
-  while (PIND & B00010000 && millis() - SlapshStartTime < 4000) {
+  while (PIN_PORT & SW_BYTE && millis() - SlapshStartTime < 4000) {
     delay(1);
   }
   lcd.clear();
@@ -138,13 +157,14 @@ void loop(void) {
           break;
         case 1:
           ain1 = ads.computeVolts(adc);
-          lcd.print(ain1);
+          digitalWrite(5, ain1 > 3.810 ? LOW : HIGH); 
+          lcd.print(((ain1 - 0.0012)  * 83165) / 10544);
           lcd.write('V');
           lcd.write((byte)B01111110);
           tmp = (ain1 * 4096) / vin;
           if (tmp != dacv) {
-            dac.setVoltage(tmp, false, 400);
-            dacv = tmp;
+            //dac.setVoltage(tmp, false, 400);
+            //dacv = tmp;
           }
           break;
         case 2:
@@ -165,7 +185,14 @@ void loop(void) {
     lcd.print("C");
   }
 
-  switchState = PIND & B00010000 ? HIGH : LOW;
+  static byte li = 0;
+  byte i = PIN_PORT & AB_BYTE;
+  if (li != i) {
+    REUpdateISR();
+    li = i;
+  }
+
+  switchState = PIN_PORT & SW_BYTE ? HIGH : LOW;
   if (last_switchState != switchState) {
     last_switchState = switchState;
     lcd.setCursor(0,1);
